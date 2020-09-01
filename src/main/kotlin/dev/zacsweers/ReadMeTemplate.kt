e317fd0ca3914dbb2ce948862c8adeec68c1104f
Ankush Gupta <me@ankushg.com>
2020-08-31 18:21:00 -0700

    Update ReadMeTemplate.kt

--- src/main/kotlin/dev/zacsweers/ReadMeTemplate.kt
+++ src/main/kotlin/dev/zacsweers/ReadMeTemplate.kt
@@ -5,7 +5,7 @@ fun createReadMe(
   blogActivity: List<ActivityItem>
 ): String {
   return """
-    Currently working on [Slack](https://slack.com/). Read [my blog](https://zacsweers.dev/) or [follow @ZacSweers on Twitter](https://twitter.com/ZacSweers).
+    Currently working on [Quizlet](https://quizlet.com/). Read [my blog](https://ankushg.com/) or [follow @ankushg on Twitter](https://twitter.com/ankushg).
 
     <table><tr><td valign="top" width="60%">
 
@@ -19,9 +19,9 @@ ${githubActivity.joinToString("\n\n") { "    $it" }}
     <!-- blog starts -->
 ${blogActivity.joinToString("\n\n") { "    $it" }}
     <!-- blog ends -->
-    More on [zacsweers.dev](https://zacsweers.dev/)
+    More on [ankushg.com](https://ankushg.com/)
     </td></tr></table>
     
-    <sub><a href="https://simonwillison.net/2020/Jul/10/self-updating-profile-readme/">Inspired by Simon Willison's auto-updating profile README.</a></sub>
+    <sub><a href="https://github.com/ZacSweers/ZacSweers">Based on Zac Sweer's auto-updating profile README</a>, which is <a href="https://simonwillison.net/2020/Jul/10/self-updating-profile-readme/">inspired by Simon Willison's auto-updating profile README.</a></sub>
   """.trimIndent()
 }
