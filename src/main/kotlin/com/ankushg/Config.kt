bf328d390e9f5f611e96382ec3d782e8f0eff535
Ankush Gupta <me@ankushg.com>
2021-04-12 12:09:07 -0400

    update blocklist to include blog upstream

--- src/main/kotlin/com/ankushg/Config.kt
+++ src/main/kotlin/com/ankushg/Config.kt
@@ -3,6 +3,7 @@ package com.ankushg
 object Config {
     val blocklistedActivityStrings = listOf(
             "forestry.io", // disable forestry CMS updates from showing
-            "ankushg/ankushg" // disable updates to personal blog or readme from showing
+            "ankushg/ankushg", // disable updates to personal blog or readme from showing
+            "mmistakes/mm-github-pages-starter" // disable accidental PRs to blog upstream
     )
 }
